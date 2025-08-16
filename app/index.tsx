855d6f730f8a47eaa63a575244a418858ad47cc8
eniobezerra <142815890+eniobezerra@users.noreply.github.com>
2025-08-16 12:32:08 +0000

    autor1 committ

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, ScrollView, Text, TextInput, View } from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue=" Autor :Enio Ramos Bezerra"
+      />
+    </ScrollView>
+  );
+};
+
+export default App;
