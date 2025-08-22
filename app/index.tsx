b44a8b45bcd26b9c6b8d4f6602127bdb0ff9815e
eniobezerra <142815890+eniobezerra@users.noreply.github.com>
2025-08-22 00:35:26 +0000

    1 commit

--- app/index.tsx
+++ app/index.tsx
@@ -7,11 +7,8 @@ const App = () => {
       <Text></Text>
       <View>
         <Text></Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{width: 200, height: 200}}
+        <Image source={require('@/assets/images/foto1.jpg')}
+        style={{width: 200, height: 200}}
         />
       </View>
       <TextInput
